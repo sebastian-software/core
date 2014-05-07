c889d59db8f8e5a2fb2fd8bc40780c643724b3d0
Sebastian Fastner <mail@sebastianfastner.de>
2014-05-07 18:17:34 +0200

    Switch over to new promises implementation that fulfills new Promises A+ test suite (v 1.1.1)

--- source/class/core/event/Promise.js
+++ source/class/core/event/Promise.js
@@ -5,229 +5,236 @@
 ==================================================================================================
 */
 
-"use strict";
-
 /**
  * Promises implementation of A+ specification passing Promises/A+ test suite.
  * http://promises-aplus.github.com/promises-spec/
+ *
+ * Based upon Native Promise Only
+ * v0.0.1-a (c) Kyle Simpson
+ * MIT License: http://getify.mit-license.org
  */
-core.Class("core.event.Promise",
-{
-	include : [core.util.MLogging],
-
-	construct : function()
-	{
-		// Initialize lists on new Promises
-		this.__onFulfilledQueue = [];
-		this.__onRejectedQueue = [];
-	},
-
-	members :
-	{
-		/** {=String} Current state */
-		__state : "pending",
-
-		/** {=Boolean} Whether the promise is locked */
-		__locked : false,
-
-		/** {=any} Any value */
-		__valueOrReason : null,
-
-		/** {=Array} Placeholder for internal fulfill queue */
-		__onFulfilledQueue : null,
-
-		/** {=Array} Placeholder for internal reject queue */
-		__onRejectedQueue : null,
 
 
-		/**
-		 * {any} Returns the value of the promise
-		 */
-		getValue : function() {
-			return this.__valueOrReason;
-		},
+(function() {
+	/*jshint validthis:true */
+	"use strict";
 
+	function schedule(fn) {
+		return (typeof setImmediate !== "undefined") ?
+			setImmediate(fn) : setTimeout(fn,0)
+		;
+	}
 
-		/**
-		 * {String} Returns the current state. One of pending, fulfilled or rejected.
-		 */
-		getState : function() {
-			return this.__state;
-		},
+	// promise duck typing?
+	function isThenable(o,shallow) {
+		var _then;
 
-
-		/**
-		 * Fulfill promise with @value {any?}.
-		 */
-		fulfill : function(value)
-		{
-			if (!this.__locked)
-			{
-				this.__locked = true;
-				this.__state = "fulfilled";
-				this.__valueOrReason = value;
-
-				core.Function.immediate(this.__execute, this);
+		if (o !== null &&
+			(
+				typeof o === "object" || typeof o === "function"
+			)
+		) {
+			if (shallow) {
+				// shallow/weak check, so that we
+				// don't fire a potential getter
+				return ("then" in o);
 			}
-		},
+			_then = o.then;
+			return isFunction(_then) ? _then : false;
+		}
+		return false;
+	}
 
+	function isFunction(fn) {
+		return (typeof fn === "function");
+	}
 
-		/**
-		 * Reject promise with @reason {String|any?}.
-		 */
-		reject : function(reason)
-		{
-			if (!this.__locked)
-			{
-				this.__locked = true;
-				this.__state = "rejected";
-				this.__valueOrReason = reason;
+	function notify() {
+		if (this.state === 0) {
+			return;
+		}
 
-				core.Function.immediate(this.__execute, this);
-			}
-		},
+		var cb, ret, _then, _chained, queue;
 
+		if (this.state === 1) {
+			queue = this.success;
+		}
+		if (this.state === 2) {
+			queue = this.failure;
+		}
 
-		/**
-		 * Executes a single fulfillment or rejection queue @entry {Array}
-		 * with the give @valueOrReason {any} and @state {String}.
-		 */
-		__executeEntry : function(entry, valueOrReason, state)
-		{
-			var child = entry[0];
-			var callback = entry[1];
-			var safe = entry[3];
-
-			if (callback == null)
-			{
-				if (state == "rejected") {
-					child.reject(valueOrReason);
-				} else {
-					child.fulfill(valueOrReason);
+		while ((cb = queue.shift())) {
+			_chained = this.chained.shift();
+			try {
+				ret = cb(this.msg);
+				if (ret === _chained.promise) {
+					throw new TypeError("Illegal promise chain cycle");
+				}
+				if ((_then = isThenable(ret))) {
+					_then.call(ret,_chained.resolve,_chained.reject);
+				}
+				else {
+					_chained.resolve(ret);
 				}
 			}
-			else
-			{
-				var retval;
-				var context = entry[2];
-				if (safe)
-				{
-					try
-					{
-						retval = context ? callback.call(context, valueOrReason) : callback(valueOrReason);
-					}
-					catch (ex) {
-						child.reject(ex);
-					}
-				}
-				else
-				{
-					retval = context ? callback.call(context, valueOrReason) : callback(valueOrReason);
-				}
-
-				if (retval && retval.then && typeof retval.then == "function")
-				{
-					var retstate = retval.getState ? retval.getState() : "pending";
-					if (retstate == "pending")
-					{
-						retval.then(function(value) {
-							child.fulfill(value);
-						}, function(reason) {
-							child.reject(reason);
-						});
-					}
-					else if (retstate == "fulfilled")
-				  {
-						child.fulfill(retval.getValue());
-					}
-					else if (retstate == "rejected")
-					{
-						child.reject(retval.getValue());
-					}
-				}
-				else
-				{
-					child.fulfill(retval);
-				}
+			catch (err) {
+				_chained.reject(err);
 			}
-		},
-
-
-		/**
-		 * Handle fulfillment or rejection of promise
-		 * Runs all registered then handlers
-		 */
-		__execute : function()
-		{
-			// Shorthands
-			var state = this.__state;
-			var queue = state == "rejected" ? this.__onRejectedQueue : this.__onFulfilledQueue;
-			var valueOrReason = this.__valueOrReason;
-
-			// Always repeat queue length check as queue could be changed within handler
-			for (var i=0; i<queue.length; i++) {
-				this.__executeEntry(queue[i], valueOrReason, state);
-			}
-
-			// Cleanup lists for next usage
-			this.__onRejectedQueue.length = this.__onFulfilledQueue.length = 0;
-		},
-
-
-		/**
-		 * {core.event.Promise} Register fulfillment handler @onFulfilled {Function}
-		 * and rejection handler @onRejected {Function} returning new child promise.
-		 * If @safe {Boolean} is set, the default value jasy.Env.getValue("safepromises")
-		 * is overwritten. If safe is false no try and catch surrounds executing functions,
-		 * so application execution is stopped due to uncatched error.
-		 */
-		then : function(onFulfilled, onRejected, context, safe)
-		{
-			var child = new core.event.Promise;
-
-			var fullfilledQueue = this.__onFulfilledQueue;
-			var rejectedQueue = this.__onRejectedQueue;
-
-			if (safe == null) {
-				safe = jasy.Env.getValue("safepromises");
-			}
-
-			if (onFulfilled && typeof onFulfilled == "function") {
-				fullfilledQueue.push([child, onFulfilled, context, safe]);
-			} else {
-				fullfilledQueue.push([child, , , safe]);
-			}
-
-			if (onRejected && typeof onRejected == "function") {
-				rejectedQueue.push([child, onRejected, context, safe]);
-			} else {
-				rejectedQueue.push([child, , , safe]);
-			}
-
-			if (this.__locked) {
-				core.Function.immediate(this.__execute, this);
-			}
-
-			return child;
-		},
-
-
-		/**
-		 * Throws all remaining errors to application level and display promise rejecting
-		 * on console if in debug mode.
-		 */
-		done : function()
-		{
-			return this.then(null, function(reason)
-			{
-				if (jasy.Env.isSet("debug"))
-				{
-					console.error("Promise rejected: ", reason);
-				}
-
-				throw reason;
-			}, null, false);
 		}
 	}
-});
 
+	function resolve(msg) {
+		var _then, self, obj;
+
+		if (this.def) {
+			if (this.triggered) {
+				return;
+			}
+			this.triggered = true;
+			self = this.def;
+		}
+		else {
+			self = this;
+		}
+
+		if (self.state !== 0) {
+			return;
+		}
+
+		obj = {
+			def: self,
+			triggered: false
+		};
+
+		try {
+			if ((_then = isThenable(msg))) {
+				_then.call(msg,resolve.bind(obj),reject.bind(obj));
+			}
+			else {
+				self.msg = msg;
+				self.state = 1;
+				schedule(notify.bind(self));
+			}
+		}
+		catch (err) {
+			reject.call(obj,err);
+		}
+	}
+
+	function fulfill(msg) {
+		resolve(msg);
+	}
+
+	function publicResolve(msg) {
+		if (this.triggered) {
+			return;
+		}
+		this.triggered = true;
+
+		resolve.call(this,msg);
+	}
+
+	function reject(msg) {
+		var self;
+
+		if (this.def) {
+			if (this.triggered) {
+				return;
+			}
+			this.triggered = true;
+			self = this.def;
+		}
+		else {
+			self = this;
+		}
+
+		if (self.state !== 0) {
+			return;
+		}
+
+		self.msg = msg;
+		self.state = 2;
+		schedule(notify.bind(self));
+	}
+
+	function publicReject(msg) {
+		if (this.triggered) {
+			return;
+		}
+		this.triggered = true;
+
+		reject.call(this,msg);
+	}
+
+	function then(success,failure) {
+		this.success.push(isFunction(success) ? success : function __default_success__(m){ return m; });
+		this.failure.push(isFunction(failure) ? failure : function __default_failure__(m){ throw m; });
+
+		var p = new Promise(function __Promise__(resolve,reject){
+			this.chained.push({
+				resolve: resolve,
+				reject: reject
+			});
+		}.bind(this));
+		this.chained[this.chained.length-1].promise = p;
+
+		schedule(notify.bind(this));
+
+		return p;
+	}
+
+	function __catch(failure) {
+		return this.promise.then(void 0,failure);
+	}
+
+	function Promise(cb) {
+		var def = {
+			promise: this,
+			state: 0,
+			triggered: false,
+			success: [],
+			failure: [],
+			chained: []
+		};
+		this.then = function(success, failure, context) {
+			if (context) {
+				if (success && typeof success == "function") {
+					success = success.bind(context);
+				}
+				if (failure && typeof failure == "function") {
+					failure = failure.bind(context);
+				}
+			}
+
+			return then.call(def, success,failure);
+		};
+		this["catch"] = __catch.bind(def);
+
+		try {
+			var resolve = publicResolve.bind(def);
+			var reject = publicReject.bind(def);
+
+			this.fulfill = resolve;
+			this.reject = reject;
+			var self = this;
+			this.done = function() {
+				return this.then(null, function(reason) {
+					if (jasy.Env.isSet("debug"))
+					{
+						console.error("Promise rejected: ", reason);
+					}
+
+					throw reason;
+				}, null, false);
+			};
+
+			cb && cb(resolve, reject);
+		}
+		catch (err) {
+			reject.call(def,err);
+		}
+	}
+
+	core.Main.declareNamespace("core.event.Promise", Promise);
+
+})();
